839f744ad775ae3f9fc6e5857c21b35989a1606e
CHRIS <chard23488@my.sunywcc.edu>
2018-06-05 14:12:06 -0400

    Add files via upload

---
+++ Christopher_Hardy_10567.cpp
@@ -0,0 +1,48 @@
+/*
+Christopher Hardy
+Program Status - Complete
+Calculates the area of a square or triangle.
+*/
+#include <iostream>
+using namespace std;
+
+float areaTriangle(float, float);
+float areaSquare(float);
+int main()
+{
+    char shape;
+
+    cout << "Enter the first letter of shape to calculate area (s or t): ";
+    cin >> shape;
+    if(shape == 's')
+    {
+        float base;
+        cout << "Enter the measurement of the side of the square: ";
+        cin >> base;
+        cout << "Area of the square is " << areaSquare(base) << "." << endl;
+    }
+    else if (shape == 't')
+    {
+        float base;
+        float height;
+        cout << "Enter the measurement of the base of the triangle: ";
+        cin >> base;
+        cout << "Enter the measurement of the height of the triangle: ";
+        cin >> height;
+        cout << "Area of the triangle is " << areaTriangle(base, height)<< "." << endl;
+    }
+    else
+    {
+        cout << "Invalid input. Try entering s or t next time?";
+    }
+    return 0;
+}
+
+float areaSquare(float side)
+{
+   return(side*side);
+}
+float areaTriangle(float base, float height)
+{
+    return(0.5*base*height);
+}
